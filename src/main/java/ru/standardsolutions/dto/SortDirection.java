7ae9fb72529205c1ff32805b612317ab6fa037d2
Merkurev Sergei <MerkurevSergei@yandex.ru>
2024-08-17 05:32:58 +0900

    [SSSF-4] Добавлены spring зависимости и rest модели чтения

---
+++ src/main/java/ru/standardsolutions/dto/SortDirection.java
@@ -0,0 +1,19 @@
+package ru.standardsolutions.dto;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.Order;
+import jakarta.persistence.criteria.Root;
+
+public enum SortDirection {
+    ASC {
+        public <T> Order build(Root<T> root, CriteriaBuilder cb, FetchRequestDto.Sort request) {
+            return cb.asc(root.get(request.getField()));
+        }
+    }, DESC {
+        public <T> Order build(Root<T> root, CriteriaBuilder cb, FetchRequestDto.Sort request) {
+            return cb.desc(root.get(request.getField()));
+        }
+    };
+
+    public abstract <T> Order build(Root<T> root, CriteriaBuilder cb, FetchRequestDto.Sort request);
+}
